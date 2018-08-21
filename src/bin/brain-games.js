37490e7102f33ce1845a59367edc033cbeb04e7c
AntonVoronezh <pstut@mail.ru>
2018-08-21 18:49:50 +0300

    error correction (step 2)

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import { userName } from '..';
-console.log('Welcome to the Brain Games!');
-console.log('Hello, ' + userName('May I have your name? '));
+import { hello } from '..';
+
+hello();
--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
 import readlineSync from 'readline-sync';
-export const userName = readlineSync.question;
+
+export const hello = () => {
+    console.log('Welcome to the Brain Games!');
+    const userName = readlineSync.question('May I have your name? ');
+    console.log('Hello!' + userName);
+  };
