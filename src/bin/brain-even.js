bdb95d359ebd9cbec0817629f0ff5b634876a055
AntonVoronezh <pstut@mail.ru>
2018-08-22 23:34:53 +0300

    (step 5)

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import playGame from '..';
+import playEven from "index";
 
-playGame();
+playEven();
--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
-
-const playGame = () => {
+/**
+ * Game Even
+ * @example
+ * Answer "yes" if number even otherwise answer "no".
+ * Question: 15
+ * Your answer: no
+ */
+const playEven = () => {
   const welcome = 'Welcome to Brain Games! \n';
   console.log(welcome);
   const rules = 'Answer "yes" if number even otherwise answer "no".';
@@ -26,4 +32,51 @@ const playGame = () => {
   };
   console.log(playRounds(0));
 };
-export default playGame;
+export default playEven;
+
+/**
+ * Game Calc
+ * @example
+ * What is the result of the expression?
+ * Question: 4 + 10
+ * Your answer: 14
+ */
+
+export const playCalc = () => {
+  const welcome = 'Welcome to Brain Games! \n';
+  console.log(welcome);
+  const rules = 'What is the result of the expression?';
+  console.log(rules);
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`\n Hello!${userName}`);
+  const playRounds = (acc) => {
+    const roundsCount = 3;
+    if (acc === roundsCount) {
+      return `\n Congratulations ${userName}!`;
+    }
+    const randomNumForExpression1 = Math.floor(Math.random() * 10);
+    const randomNumForExpression2 = Math.floor(Math.random() * 10);
+    const outputTotheScreen = (expressionForQuestion) => {
+      const userAnswer = readlineSync.question('Your answer: ');
+      const isEven = Math.floor(expressionForQuestion) === Math.floor(userAnswer);
+      if (isEven) {
+        console.log('Correct!');
+        return playRounds(acc + 1);
+      }
+      return `'${userAnswer}' is wrong answer ;(. Correct answer was '${expressionForQuestion}'. \n Let's try again, ${userName}`;
+    };
+
+    if (acc === 0) {
+      console.log(`\n Question: ${`${randomNumForExpression1} + ${randomNumForExpression2}`}`);
+      return outputTotheScreen(randomNumForExpression1 + randomNumForExpression2);
+    } if (acc === 1) {
+      console.log(`\n Question: ${`${randomNumForExpression1} - ${randomNumForExpression2}`}`);
+      return outputTotheScreen(randomNumForExpression1 - randomNumForExpression2);
+    } if (acc === 2) {
+      console.log(`\n Question: ${`${randomNumForExpression1} * ${randomNumForExpression2}`}`);
+      return outputTotheScreen(randomNumForExpression1 * randomNumForExpression2);
+    }
+    return undefined;
+  };
+  console.log(playRounds(0));
+};
