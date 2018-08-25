9993ea1a5caafe44b1893d5454c1a4e8a097abf9
AntonVoronezh <pstut@mail.ru>
2018-08-25 15:49:29 +0300

    error correction (step 7 #2)

--- src/games/balance.js
+++ src/games/balance.js
@@ -1,7 +1,7 @@
 import buildGame from '..';
 import getRandomNum from '../utils';
-import { cons, car, cdr } from 'hexlet-pairs';
-/**
+import { cons } from 'hexlet-pairs';
+/** toString
  * Game Balance
  * @example
  * Balance the given number.
@@ -10,59 +10,36 @@ import { cons, car, cdr } from 'hexlet-pairs';
  * Correct!
  */
 const description = 'Balance the given number';
-const summa = (num) => {
-  const str = String(num);
-  let big = 0;
-  for (let i = 0; i <= str.length - 1; i += 1) {
-    if (Number(str[i]) > big) { big = Number(str[i]); }
-  }
-  let small = big;
-  for (let i = 0; i <= str.length - 1; i += 1) {
-    if (Number(str[i]) < small) { small = Number(str[i]); }
-  }
-  return cons(small, big);
-};
-const changesNum = (num, pair) => {
-  const str = String(num);
-  let result = '';
-  const small = car(pair) + 1;
-  const big = cdr(pair) - 1;
-  let bigCount = 0;
-  let smallCount = 0;
-  for (let i = 0; i <= str.length - 1; i += 1) {
-    if (Number(str[i]) === car(pair) && smallCount === 0) {
-      result += small; smallCount += 1;
-    } else if (Number(str[i]) === cdr(pair) && bigCount === 0) {
-      result += big; bigCount += 1;
-    } else {
-      result += Number(str[i]);
-    }
-  }
-  return result;
-};
-const isOne = arg => cdr(arg) - car(arg) === 1;
-const bubbleSort = (number) => {
-  const num = String(number).split('');
-  const n = num.length;
-  for (let i = 0; i < n - 1; i += 1) {
-    for (let j = 0; j < n - 1 - i; j += 1) {
-      if (num[j + 1] < num[j]) {
-        const t = num[j + 1];
-        num[j + 1] = num[j];
-        num[j] = t;
-      }
-    }
-  }
-  return num.join('');
-};
-const getBalance = (num) => {
-  const out = changesNum(num, summa(num));
-  if (isOne(summa(out))) { return bubbleSort(out); }
-  return getBalance(out);
-};
 const generateQuestion = () => {
-  const question = getRandomNum(100, 999);
-  const correctAnswer = getBalance(question);
-  return cons(Math.abs(question), Math.abs(correctAnswer));
+  const randNum = getRandomNum(100, 999);
+  let num = randNum;
+  let count = 0;
+  let summ = 0;
+  while (num !== 0) {
+    summ += num % 10;
+    // console.log('summ - ' + summ);
+    count += 1;
+    // console.log('count - ' + count);
+    num = Math.floor(num / 10);
+  }
+  const integerPart = Math.floor(summ / count);
+  // console.log('\n integerPart - ' + integerPart);
+  let remainder = summ % count;
+  // console.log('remainder - ' + remainder);
+  let result = 0;
+  for (let i = 0; i < count; i += 1) {
+    result += integerPart * (10 ** i);
+    if (remainder !== 0) {
+      result += 1 * (10 ** i);
+      remainder -= 1;
+    }
+    // console.log('result - ' + i +' -> ' + result);
+  }
+  const getQuestion = `${randNum}`;
+  const correctAnswer = `${result}`;
+  // console.log(getQuestion);
+  // console.log(correctAnswer);
+  return cons(getQuestion, correctAnswer);
 };
+// console.log(toString(generateQuestion(4181)));
 export default () => buildGame(description, generateQuestion);
