266ea733bd6e14c60825d45ca00a93ed06bcb922
AntonVoronezh <pstut@mail.ru>
2018-08-26 14:51:38 +0300

    correction (step 9)

--- src/games/prime.js
+++ src/games/prime.js
@@ -14,7 +14,7 @@ const isPrime = (number) => {
   if (number < 2) {
     return false;
   }
-  for (let i = 2; i < number - 1; i += 1) {
+  for (let i = 2; i < Math.sqrt(number); i += 1) {
     if (number % i === 0) {
       return false;
     }
